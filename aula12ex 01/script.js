1063261163a75c16cb45e2a669bdd1ede8259763
RAFAEL DORCINO <163371790+Rafael-Dorcino@users.noreply.github.com>
2024-09-17 05:53:14 -0300

    Horários do dia

--- aula12ex 01/script.js
+++ aula12ex 01/script.js
@@ -3,6 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date ()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora}:00 horas.`
 
+    if(hora >= 0 && hora < 12) {
+        //Bom dia 
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#a6caf0'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#ad7263'
+    } else {
+        //Boa noite
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#1d1617'
+    }
 }
