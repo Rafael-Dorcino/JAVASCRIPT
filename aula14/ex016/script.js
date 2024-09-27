ca6445ed8c59393f9d2629fafa824df3f6e2034d
RAFAEL DORCINO <163371790+Rafael-Dorcino@users.noreply.github.com>
2024-09-26 22:30:56 -0300

    tabuada e calculo com passos

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -1,14 +1,36 @@
-
-
 function contar(){
-    var ni = window.document.getElementById('inicio').value
-    var nf = document.querySelector('input#fimn').value
-    var np = document.getElementById('passo').value
-    /*res.innerHTML = `${ni} - ${nf} - ${np}`*/
+    var ni = document.getElementById('inicio')
+    var nf = document.querySelector('input#fimn')
+    var np = document.getElementById('passo')
+    let res = document.getElementById('res')
 
-    while(ni <= nf){
-        
-        ni = ni + np
-        res.innerHTML += `<p>${ni}</p>`
+    if (ni.value.length == 0 || nf.value.length == 0 || np.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[Erro] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+
+        var i = Number(ni.value)
+        var f = Number(nf.value)
+        var p = Number(np.value)
+
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem Crescente
+            for(var c = i; c <= f; c +=p) {
+                res.innerHTML += ` &#x1F449 ${c} `
+            }
+        } else {
+            //Contagem Regressiva 
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += ` &#x1F449 ${c}`
+            }
+        }
+        res.innerHTML += '&#x1F3C1'
     }
+    
 }
--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -1,8 +1,23 @@
 function calcular(){
-    var nume = document.getElementById('num').value
-
-    for(var n2 = 0; n2 <= 10; n2++) {
-        var s = nume * n2
-        res.innerHTML += `<p> ${nume} x ${n2} = ${s} </p>`
+    var num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
+
+    /* for(var n2 = 0; n2 <= 10; n2++) {
+        var s = num * n2
+        res.innerHTML += `<p> ${num} x ${n2} = ${s} </p>`
+    } */ 
 }
